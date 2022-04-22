0e20a3fc65fa07e25cfb99d3c664bb088cd39339
Vasko Bozinovski <vbozinovski16@gmail.com>
2022-04-22 20:55:47 +0200

    Added pagination; FINISHED

--- src/components/Categories/categories.js
+++ src/components/Categories/categories.js
@@ -3,6 +3,7 @@ import React from "react";
 const Categories = () => {
     return (
         <div>
+            <h2>Categories</h2>
             <ul>
                 <li>NOVEL</li>
                 <li>THRILLER</li>
